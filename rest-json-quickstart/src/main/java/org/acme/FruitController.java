e18294024159290363b7871c702528082bb2df94
Pavel Vorobev <p.vorobyev@inbox.ru>
2020-10-01 19:10:26 +0300

    rest-json-quickstart

---
+++ rest-json-quickstart/src/main/java/org/acme/FruitController.java
@@ -0,0 +1,32 @@
+package org.acme;
+
+import io.vertx.core.impl.ConcurrentHashSet;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import java.io.FileNotFoundException;
+import java.util.Set;
+
+@Path("/fruits")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
+public class FruitController {
+
+    private static Set<Fruit> fruits = new ConcurrentHashSet<>();
+
+    static {
+        fruits.add(new Fruit("Apple", "Antonovka"));
+        fruits.add(new Fruit("Plum", "Ripe plums"));
+    }
+
+    @GET
+    public Set<Fruit> list() throws FileNotFoundException {
+        return fruits;
+    }
+
+    @POST
+    public Set<Fruit> add(Fruit fruit) {
+        fruits.add(fruit);
+        return fruits;
+    }
+}
